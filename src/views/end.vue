ec290409d7d2025e615f762eee2b6aabffe10eb5
zhoulf <15658134760@163.com>
2020-05-21 21:09:23 +0800

    --

--- src/views/end.vue
+++ src/views/end.vue
@@ -19,41 +19,143 @@
     </div>
     <div class="end-foot again" @click="goclick(1)"></div>
     <div class="end-foot goback" @click="goclick(2)"></div>
+    <van-popup v-model="popshow" :close-on-click-overlay="false" class="popclass">
+      <div class="mathchloding">匹配中...</div>
+    </van-popup>
+    <van-dialog v-model="matchshow" :showConfirmButton="false" class="vantdia">
+      <div class="match-dialog">
+        <div class="match-top">
+          <img src="../assets/img/fdj.png" />
+          <p class="span0">正在寻找对手({{number}})</p>
+        </div>
+        <div class="match-center">
+          <div class="match-center-left">
+            <img :src="myUser.litpicPath ? myUser.litpicPath : defaultHead" />
+            <span class="span0">{{myUser.nickname ? myUser.nickname : 'xxx'}}</span>
+          </div>
+          <div class="match-center-center">vs</div>
+          <div class="match-center-left">
+            <img :src="otherInfos.litpicPath ? otherInfos.litpicPath : defaultSrc" />
+            <span class="span0">{{otherInfos.opponentName ? otherInfos.opponentName : '***'}}</span>
+          </div>
+        </div>
+        <div class="match-footer" @click="closematch"></div>
+      </div>
+    </van-dialog>
   </div>
 </template>
-<script lang="ts">
+<script>
+import Vue from "vue";
+import { Dialog, Toast, Popup } from "vant";
+Vue.use(Dialog)
+  .use(Toast)
+  .use(Popup);
 export default {
   name: "end",
   data() {
     return {
+      matchshow: false,
+      popshow: false,
+      number: 0,
+      timer: null,
       endType: null,
       myGrad: localStorage.getItem("myGrad"),
+      defaultSrc: require("@/assets/img/gif1.gif"),
       otherGrade: localStorage.getItem("otherGrade"),
       otherInfo: JSON.parse(localStorage.getItem("otherInfo")),
       myUser: JSON.parse(localStorage.getItem("myUser")),
-      defaultHead: require("@/assets/img/head.png")
+      defaultHead: require("@/assets/img/head.png"),
+      myInfo: {},
+      otherInfos: {}
     };
   },
   methods: {
     goclick(key) {
-      this.$router.push({
-        path: "home",
-        query:{
-          status:key==1 ? true : false
+      if (key == 1) {
+        this.beginTime();
+      } else {
+        this.$router.push({
+          path: "home"
+        });
+      }
+    },
+    beginTime() {
+      this.popshow = true;
+      if (typeof WebSocket === "undefined") {
+        alert("您的浏览器不支持socket");
+      }
+      window.ws = new WebSocket(
+        // "ws://192.168.10.2:8123/websocket/" + localStorage.getItem("token")
+        "ws://47.110.91.228:8123/websocket/" + localStorage.getItem("token")
+      );
+      window.ws.onopen = this.onopen;
+      window.ws.onmessage = this.onmessage;
+      window.ws.onerror = this.onerror;
+    },
+    onopen(e) {
+      console.log("连接成功");
+      this.popshow = false;
+      this.matchshow = true;
+      this.timer = setInterval(this.goTime, 1000);
+    },
+    onmessage(data) {
+      let obj = JSON.parse(data.data);
+      this.otherInfo = obj.opponentUser;
+      this.myUser = obj.myUser;
+      localStorage.setItem("otherInfo", JSON.stringify(this.otherInfos));
+      localStorage.setItem("myUser", JSON.stringify(this.myUser));
+      localStorage.setItem("questionList", JSON.stringify(obj.questionList));
+      localStorage.setItem("playUserId", obj.playUserId);
+      Toast({
+        message: "匹配成功",
+        onOpened: () => {
+          this.closematch();
+          this.$router.push({
+            path: "match"
+          });
         }
       });
+    },
+    onerror(e) {
+      console.log(e);
+    },
+    goTime() {
+      this.number++;
+    },
+    clearTimer() {
+      clearInterval(this.timer);
+      this.number = 0;
+    },
+    closematch() {
+      window.ws.onclose = this.onclose;
+      this.clearTimer();
+      this.matchshow = false;
+    },
+    onclose() {
+      console.log("断开链接");
     }
   },
   mounted() {
-    let status =this.$route.query.status
+    let status = this.$route.query.status;
     if (status) {
-      this.endType = status
+      this.endType = status;
     } else {
-      this.endType = 'false';
+      this.endType = "false";
+    }
+  },
+  beforeDestroy() {
+    clearInterval(this.timer);
+    if (window.wx) {
+      window.ws.onclose = this.onclose;
     }
   }
 };
 </script>
+<style lang="scss">
+.vantdia {
+  background: #ccc;
+}
+</style>
 <style lang="scss" scoped>
 .end {
   width: 92%;
@@ -139,6 +241,72 @@ export default {
       background: url(../assets/img/endgo.png) no-repeat center center;
       background-size: 100% 100%;
     }
+  }
+  .match-dialog {
+    height: calc(100vh - 120px);
+    margin: 0 auto;
+    background: url(../assets/img/matchbg.png) no-repeat center center;
+    background-size: 100% 100%;
+    position: relative;
+    overflow: hidden;
+    .match-top {
+      width: 60%;
+      height: 40px;
+      margin: 40px auto;
+      background: url(../assets/img/ppbg.png) no-repeat center center;
+      background-size: 100% 100%;
+      display: flex;
+      flex-direction: row;
+      justify-content: space-around;
+      align-items: center;
+      img {
+        width: 30px;
+        height: 30px;
+      }
+    }
+    .match-center {
+      width: 80%;
+      margin: 0 auto;
+      margin-top: 40px;
+      display: flex;
+      justify-content: space-around;
+      align-items: center;
+      &-left {
+        display: flex;
+        flex-direction: column;
+        align-items: center;
+        img {
+          width: 80px;
+          height: 80px;
+          border-radius: 50%;
+          margin-bottom: 10px;
+        }
+      }
+      &-center {
+        font-weight: 700;
+        font-size: 22px;
+        color: #fff;
+      }
+    }
+    .match-footer {
+      position: absolute;
+      left: 50%;
+      bottom: 40px;
+      transform: translateX(-50%);
+      width: 40%;
+      height: 40px;
+      background: url(../assets/img/nopp.png) no-repeat center center;
+      background-size: contain;
+      cursor: pointer;
+    }
+  }
+  .popclass {
+    width: 80%;
+    height: 100px;
+    font-size: 20px;
+    font-weight: bolder;
+    text-align: center;
+    line-height: 100px;
   }
 }
 </style>
